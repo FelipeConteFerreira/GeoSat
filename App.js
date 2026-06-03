b691976042a633bb55db5658ce9fbcd87a069dbe
Felipe Conte <felipeconteferreira@gmail.com>
2026-06-03 18:56:02 -0300

    implementando no app perfil com formulario

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { PlantacaoProvider } from './context/PlantacaoContext';
+import { UsuarioProvider } from './context/UsuarioContext';
 import StackNavigator from './navigation/StackNavigator';
 
 export default function App() {
   return (
-    <PlantacaoProvider>
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
-    </PlantacaoProvider>
+    <UsuarioProvider>
+      <PlantacaoProvider>
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </PlantacaoProvider>
+    </UsuarioProvider>
   );
 }
